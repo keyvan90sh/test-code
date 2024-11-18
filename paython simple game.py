4fdd0962091e1dcaab8efd67ddd4084a9e13c2a0
keyvan90sh <t53403882@gmail.com>
2024-11-18 20:15:39 +0330

    Add files via upload

---
+++ paython simple game.py
@@ -0,0 +1,12 @@
+# Simple game: Guess the number
+import random
+number = random.randint(1, 100)
+guess = None
+while guess != number:
+    guess = int(input("Guess a number between 1 and 100: "))
+    if guess < number:
+        print("Too low!")
+    elif guess > number:
+        print("Too high!")
+    else:
+        print("Congratulations! You guessed it!")
