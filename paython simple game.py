f01a75bcdd16869bdec3a3870dcf0bb5090d761f
helia heidari <helia_heidari@yahoo.com>
2024-11-18 20:22:39 +0330

    Update paython simple game.py
    change in if statement

--- paython simple game.py
+++ paython simple game.py
@@ -5,7 +5,7 @@ guess = None
 while guess != number:
     guess = int(input("Guess a number between 1 and 100: "))
     if guess < number:
-        print("Too low!")
+        print("youre stupid!")
     elif guess > number:
         print("Too high!")
     else:
